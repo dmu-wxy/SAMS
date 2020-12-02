462883685e09cb1b6ba511a11e320ec22217feb9
clever_WangErgou <48337433+dmu-wxy@users.noreply.github.com>
2020-12-02 23:12:34 +0800

    feat 添加错误页面

--- src/main/java/com/wxy/sams/Exception/MyCustomException.java
+++ src/main/java/com/wxy/sams/Exception/MyCustomException.java
@@ -3,12 +3,15 @@ package com.wxy.sams.Exception;
 import org.apache.ibatis.javassist.NotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * ControllerAdvice作用：
@@ -19,6 +22,11 @@ import java.io.PrintWriter;
 @ControllerAdvice
 public class MyCustomException {
 
+    /**
+     * 全局处理异常，处理文件上传大小过大异常
+     * @param e
+     * @param response
+     */
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public void maxUploadSizeExceededException(MaxUploadSizeExceededException e, HttpServletResponse response){
         response.setContentType("text/html;charset=utf-8");
@@ -33,5 +41,18 @@ public class MyCustomException {
             if(writer != null)
                 writer.close();
         }
+    }
+
+    /**
+     * 全局数据，在任意的controller中都可以用，
+     * map使用model.asMap()获得
+     * @return
+     */
+    @ModelAttribute(value = "info")
+    public Map<String,Object> mydata(){
+        Map<String,Object> map = new HashMap<>();
+        map.put("name","meteor");
+        map.put("adress","www.smartdog.top");
+        return map;
     }
 }
