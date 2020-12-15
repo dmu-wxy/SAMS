0f47401729189a0929f1f5441f3a674b4550382f
clever_WangErgou <48337433+dmu-wxy@users.noreply.github.com>
2020-12-15 22:09:55 +0800

    fix 让spring security支持使用iframe

--- src/main/java/com/wxy/sams/config/securityConfig.java
+++ src/main/java/com/wxy/sams/config/securityConfig.java
@@ -4,22 +4,17 @@ package com.wxy.sams.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 
 @Configuration
 public class securityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-            .antMatchers("/res/**", "/admin", "/thirdparty/**", "/auth/login").permitAll()
-            .antMatchers("/admin/**").hasAuthority("admin:index")
-            .anyRequest().authenticated()
-            .and()
-            .formLogin().loginPage("/admin").permitAll()
-            .and()
-            .logout().logoutUrl("/admin/logout").logoutSuccessUrl("/admin").invalidateHttpSession(true)
-            .and()
-            .csrf().disable()
-            .headers().frameOptions().sameOrigin();
+        http.authorizeRequests((requests) -> {
+            ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated();
+        });
+        http.formLogin();
+        http.httpBasic();
+        http.headers().frameOptions().sameOrigin();
     }
 }
