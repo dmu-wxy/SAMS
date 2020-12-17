976b4f0269cb1a6f3b685940d01612746e1b505a
clever_WangErgou <48337433+dmu-wxy@users.noreply.github.com>
2020-12-17 23:02:22 +0800

    feat 密码加密

--- src/main/java/com/wxy/sams/config/securityConfig.java
+++ src/main/java/com/wxy/sams/config/securityConfig.java
@@ -1,20 +1,51 @@
 package com.wxy.sams.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 public class securityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+//        http
+//            .authorizeRequests()   //开启配置
+//            .antMatchers("/res/**", "/admin", "/thirdparty/**", "/auth/login").permitAll() //设置路径
+//            .antMatchers("/admin/**").hasAuthority("admin:index") //hasAuthority 必须有。。。角色
+//            .anyRequest().authenticated()  //其他路径认证之后就能访问
+//            .and()
+//            .formLogin().loginPage("/admin").permitAll() //登录表单   处理登录请求的url  允许所有
+//            .and()
+//            .logout().logoutUrl("/admin/logout").logoutSuccessUrl("/admin").invalidateHttpSession(true)
+//            .and()
+//            .csrf().disable()  // 要用postman接口测试，关闭csrf攻击  登录是post请求
+//            .headers().frameOptions().sameOrigin();
         http.authorizeRequests((requests) -> {
             ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated();
         });
         http.formLogin();
         http.httpBasic();
         http.headers().frameOptions().sameOrigin();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    protected void confiure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("meteor").password("$2a$10$9BisuXCe6ZFRorvgs1CFiOjN/PR0/EqcUod7h5NBb3NVCbgwoIdRi").roles("admin")
+                .and()
+                .withUser("smartdog").password("$2a$10$/55kdJmt.NMyeFdaCJR0w.rfYpHvs1Na7MSz4pCQxEvdU27yr4uB6").roles("user");
     }
 }
--- src/test/java/com/wxy/sams/SamsApplicationTests.java
+++ src/test/java/com/wxy/sams/SamsApplicationTests.java
@@ -4,6 +4,7 @@ import com.wxy.sams.service.ManagerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class SamsApplicationTests {
@@ -14,6 +15,13 @@ class SamsApplicationTests {
     @Test
     void contextLoads() {
         System.out.println(managerService.findByEmail("2290502632@qq.com"));
+    }
+
+    @Test
+    void createPassword(){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("123456"));
+        System.out.println(encoder.encode("111111"));
     }
 
 }
