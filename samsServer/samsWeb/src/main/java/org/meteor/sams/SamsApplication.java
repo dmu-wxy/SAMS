4f94e566ebb1bc7f436d70ba55d3768ce9548843
clever_WangErgou <48337433+dmu-wxy@users.noreply.github.com>
2021-03-21 18:44:50 +0800

    feat 更新数据库结构和数据

--- samsServer/samsWeb/src/main/java/org/meteor/sams/SamsApplication.java
+++ samsServer/samsWeb/src/main/java/org/meteor/sams/SamsApplication.java
@@ -3,6 +3,8 @@ package org.meteor.sams;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -10,10 +12,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @MapperScan("org.meteor.sams.mapper")
 @EnableCaching
 @EnableScheduling
-public class SamsApplication {
+public class SamsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SamsApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SamsApplication.class);
+    }
 }
